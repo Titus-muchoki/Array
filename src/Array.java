54e708523b7447758cfc632af6fc505f6ebca497
kajela <titoyut56@gmail.com>
2022-05-10 06:09:20 -0400

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,15 @@
+
+
+class Array {
+    static void myDream() {
+        System.out.println("programming is not easy but i still have to get through");
+    }
+    public void softwareDev() {
+        System.out.println("becoming a software developer is not easy but finally it will pay");
+    }
+    public static void main(String[] args) {
+        myDream();
+Array myObj = new Array();
+myObj.softwareDev();
+    }
+}
